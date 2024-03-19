5879d8a6af98d88435a94f95602a79bea05eb496
daniel04soden <dsoden09@gmail.com>
2024-03-19 09:08:34 +0000

    made some fixes to the bx.py code

--- bx.py
+++ bx.py
@@ -1,5 +1,5 @@
 # Author: Daniel Soden
-# Purpose: BX Lab exam 
+# Purpose: BX Lab exam
 
 # Import statements
 
@@ -7,34 +7,39 @@ from myfunctions import *
 
 # Actual functions
 
-def print_languages_and_popularity(list_programming_languages:list,list_popularity:list):
+
+def print_languages_and_popularity(
+    list_programming_languages: list, list_popularity: list
+):
     """This function prints the languages and popularity of
-        a list of programming languages 
+    a list of programming languages
 
-        param: list_programming_languages (list): list of programming languages
-                list_popularity (list): list of populaity
+    param: list_programming_languages (list): list of programming languages
+            list_popularity (list): list of populaity
 
-        returns: None
+    returns: None
     """
 
-    print(f"{'Languages':^15}{'Popularity':^15}\n"
-        f"{'='*30}\n")
+    print(f"{'Languages':^15}{'Popularity':^15}\n" f"{'='*30}\n")
     count = 0
 
     for i in list_programming_languages:
         print(f"{i:<15}{list_popularity[count]:<15}")
         count += 1
 
-def update_popularity(language:str,list_programming_languages:list,list_popularity:list):
-    """ this function takes the name of a language 
+
+def update_popularity(
+    language: str, list_programming_languages: list, list_popularity: list
+):
+    """this function takes the name of a language
         and access the list of both programming languages and popularity of these languages
-        and from here edits the popularity from user input 
+        and from here edits the popularity from user input
 
         param: language (str): name of the language
         list_programming_languages (list): list of programming languages
-        list_popularity (list): list of poplar programming languages 
+        list_popularity (list): list of poplar programming languages
 
-    returns: updated popularity list 
+    returns: updated popularity list
     """
     if language in list_programming_languages:
         different_popularity = input("Enter how popular the language is now: ")
@@ -44,43 +49,43 @@ def update_popularity(language:str,list_programming_languages:list,list_populari
     else:
         pass
 
-
     return list_popularity
 
-def print_popular_language(languages:list,popularity:list):
-    """this function takes both the languages and how popular they are 
-       and returns the most popular language 
 
-       param: languages (list): list of programming languages
-              poplarity (list): the status of each languages popularity
+def print_popular_language(languages: list, popularity: list):
+    """this function takes both the languages and how popular they are
+    and returns the most popular language
 
-        returns: high_language (list): the names of the most popular languages
+    param: languages (list): list of programming languages
+           poplarity (list): the status of each languages popularity
+
+     returns: high_language (list): the names of the most popular languages
 
     """
 
     high_language = []
-    for i,status in enumerate(popularity):
-        if status == 'High':
+    for i, status in enumerate(popularity):
+        if status == "High":
             high_language.append(languages[i])
         else:
             pass
 
-    print(f"The most popular langauges are currently: \n")
-    for i,languages in enumerate(high_language):
+    print("The most popular langauges are currently: \n")
+    for i, languages in enumerate(high_language):
         print(f"{(i+1):<15}{languages:<15}\n")
 
 
-# Output 
+# Output
+
 
 def main():
-    list_programming_languages = ["Python","Java","C++","JavaScript","Ruby"]
-    list_popularity = ["High","High","Medium","High","Low"]
+    list_programming_languages = ["Python", "Java", "C++", "JavaScript", "Ruby"]
+    list_popularity = ["High", "High", "Medium", "High", "Low"]
     language = get_non_empty_string("Enter a programming language: ")
-    update_popularity(language,list_programming_languages,list_popularity)
-    print_popular_language(list_programming_languages,list_popularity)
-    print_languages_and_popularity(list_programming_languages,list_popularity)
+    update_popularity(language, list_programming_languages, list_popularity)
+    print_popular_language(list_programming_languages, list_popularity)
+    print_languages_and_popularity(list_programming_languages, list_popularity)
+
 
 if __name__ == "__main__":
     main()
-
-
